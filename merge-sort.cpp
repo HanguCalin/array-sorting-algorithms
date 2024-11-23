bc95c4c9a883b9600487062b1297ef748521acfe
Hangu Calin <hangucalin14@gmail.com>
2024-11-23 20:46:53 +0200

    Removed bad files

--- merge-sort.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main() {
-	cout << "This is not a merge sort!" << '\n';
-	return 0;
-}
-
--- mergesort.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main() {
-	cout << "This is not the Merge Sort algorithm!" << '\n';
-
-	return 0;
-}
